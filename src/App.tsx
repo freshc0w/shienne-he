d6a36e91ae9ccc734b74637969a63438e6f0d2ee
freshc0w <121275444+freshc0w@users.noreply.github.com>
2025-11-19 13:20:34 +1000

    Init images and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 const App = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <div className="bg-black text-white w-[100vw] h-[100vh]">
+    testing
+  </div>;
 };
 
 export default App;
---
+++ src/constants.ts
@@ -0,0 +1,30 @@
+export const IMAGES = {
+  image_1: {
+    src: "/image_1.jpg",
+    alt: "image_1",
+  },
+  image_2: {
+    src: "/image_2.jpg",
+    alt: "image_2",
+  },
+  image_3: {
+    src: "/image_3.jpg",
+    alt: "image_3",
+  },
+  image_4: {
+    src: "/image_4.jpg",
+    alt: "image_4",
+  },
+  image_5: {
+    src: "/image_5.jpg",
+    alt: "image_5",
+  },
+  image_6: {
+    src: "/image_6.jpg",
+    alt: "image_6",
+  },
+  image_7: {
+    src: "/image_7.jpeg",
+    alt: "image_7",
+  },
+};
