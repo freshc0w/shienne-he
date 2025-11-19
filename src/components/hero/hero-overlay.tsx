22ff046dfb26f54766ef35aa5ade05b7e5dd852c
freshc0w <121275444+freshc0w@users.noreply.github.com>
2025-11-19 14:31:52 +1000

    Apply image filters

--- src/components/hero/hero-overlay.tsx
+++ src/components/hero/hero-overlay.tsx
@@ -8,13 +8,13 @@ export const HeroOverlay = ({
   selectedImage: { src: string; alt: string };
 }) => {
   return (
-<div className={cn("bg-black relative", className)}>
-  <img
-    src={selectedImage.src}
-    alt={selectedImage.alt}
-    className="w-full h-full object-cover object-center brightness-90"
-  />
-  <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/60" />
-</div>
+    <div className={cn("bg-black relative", className)}>
+      <img
+        src={selectedImage.src}
+        alt={selectedImage.alt}
+        className="w-full h-full object-cover object-center brightness-85 contrast-120 saturate-90"
+      />
+      <div className="absolute inset-0 bg-gradient-to-b from-black/20 via-transparent to-black/70" />
+    </div>
   );
 };
